10f2f9d1394a2ae2875e6404e889c40c4bf7918d
aabbell <abelalemneh68@gmail.com>
2024-04-13 11:57:50 +0300

    script.js

---
+++ script.js
@@ -0,0 +1,41 @@
+
+const one = document.querySelector(".one");
+one.addEventListener('click',function(){
+   
+})
+
+function add( x , y) {
+    x = 5
+    y = 3
+    let result = x + y
+    return result
+}
+
+
+function subtract (){
+    let x = 0
+    let y = 0
+    let result = x + y
+    return result
+}
+
+function multiply (){
+    let x = 0
+    let y = 0
+    let result = x * y
+    return result
+}
+
+function divide(){
+    let x = 0
+    let y = 0
+    let result = x / y
+    return result 
+}
+
+function operate () {
+    add ()
+    subtract()
+    multiply()
+    divide()
+}
